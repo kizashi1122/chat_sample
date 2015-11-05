dc9206267e4337600fc9f441260f88c4bff55d16
Kizashi Nagata <kizashi1122@gmail.com>
2015-11-05 19:39:10 +0900

    簡単なChatシステム

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.2.1'
+# Use postgresql as the database for Active Record
+gem 'pg'
+
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier', '>= 1.3.0'
+# Use SCSS for stylesheets
+gem 'sass-rails', '~> 5.0'
+
+# Use CoffeeScript for .coffee assets and views
+gem 'coffee-rails', '~> 4.1.0'
+
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 2.0'
+
+group :production do
+  # Engineyard Addons
+  gem 'ey_config'
+  gem 'unicorn'
+end
---
+++ node/app.js
@@ -0,0 +1,31 @@
+var SocketIO = require('socket.io');
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  console.log('request: ' + req);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Socket Server');
+});
+
+var io = new SocketIO(server);
+server.listen(3001, function() { console.log('Server listening at port %d', 3001) });
+
+//Websocket接続を保存しておく
+var connections = [];
+
+//接続時
+io.sockets.on('connect', function (ws) {
+    //配列にWebSocket接続を保存
+    connections.push(ws);
+    //切断時
+    ws.on('disconnect', function () {
+        connections = connections.filter(function (conn, i) {
+            return (conn === ws) ? false : true;
+        });
+    });
+    //メッセージ送信時
+    ws.on('message', function (message) {
+      console.log('message:', message);
+      io.sockets.emit("broadcast", {value: message});
+    });
+});
