1a65fca7007d9c73be3f1d462d7cadc65bb8cb22
Kizashi Nagata <kizashi1122@gmail.com>
2015-11-05 20:59:14 +0900

    明示的に階層を指定

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -4,5 +4,5 @@ on_app_servers() {
   run "cd #{config.release_path}/node; npm install socket.io"
   run "pm2 delete ws"
   # copy new app dir and start ws
-  run "pm2 start app.js --name 'ws'"
+  run "cd #{config.release_path}/node; pm2 start app.js --name 'ws'"
 }
